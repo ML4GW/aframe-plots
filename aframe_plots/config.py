5ee66a74cd6b9b38b010410eb801ab7e1b1a6c6a
Alec Gunny <alec.gunny@gmail.com>
2023-08-11 19:24:19 -0500

    adding mdc comparison notebook

--- aframe_plots/config.py
+++ aframe_plots/config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # TODO: define things relative to this
-root = Path(os.getenv("AFRAME_PLOT_DATA"))
+root = Path(os.getenv("AFRAME_PLOT_DATA", "/"))
 
 
 class DataPaths:
@@ -13,6 +13,11 @@ class DataPaths:
     paths for the time being
     """
     INTERVAL_PLOTS: Path = Path("/home/ethan.marx/aframe/interval-runs")
+    MDC_RESULTS: Path = Path("/home/alec.gunny/ml-mdc/comp-results")
+    AFRAME_MDC_RESULTS: Path = Path(
+        "/home/alec.gunny/ml-mdc/results/hp_search_230801/"
+        "GPU-2/iteration_1/evaluation.hdf"
+    )
 
     # e.g.
     # INTERVAL_PLOTS = root / "interval-runs"
